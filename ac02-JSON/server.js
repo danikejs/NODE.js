a18fe7d9b2f0b54ac9327829804de43c868fed31
Abdalla Dib <159274883+danikejs@users.noreply.github.com>
2025-03-24 13:34:48 -0300

    Add files via upload

--- ac02-JSON/server.js
+++ ac02-JSON/server.js
@@ -1,39 +1,70 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { captureRejectionSymbol } = require('events');
 
 const app = express();
 const port = 3000;
-
 const artigosPath = path.join(__dirname, 'artigos.json');
 
+//filtrar nome 
 
-
-//filtar
 const artigosData = fs.readFileSync(artigosPath, 'utf-8');
 const artigos = JSON.parse(artigosData);
 
-function buscarArtigos(nome){
+function buscarArtigos(nome) {
     return artigos.find(artigo =>
         artigo.nome.toLowerCase() === nome.toLowerCase());
 }
 
-app.get('/buscar-artigo/:nome', (req,res) => {
+app.get('/buscar-artigo/:nome', (req, res) => {
     const nomeDoArtigoBuscado = req.params.nome;
     const artigoEncontrado = buscarArtigos(nomeDoArtigoBuscado);
 
-    if(artigoEncontrado){
-        res.send(`<h1>artigo encontrado:</h1><pre> ${JSON.stringify (artigoEncontrado,null,2)}</pre>`);
+    if (artigoEncontrado) {
+        res.send(`<h1>artigo encontrado:</h1><pre> ${JSON.stringify(artigoEncontrado, null, 2)}</pre>`);
     } else {
         res.send('<h1>artigo não encontrado.</h1>');
     }
 });
 
-//add
+//filtrar todos
+app.get('/todos-artigos', (req, res) => {
+    res.send(artigos);
+});
 
 
 
-app.listen(port,() =>{
-    console.log(`servidor iniciado em http://localhost:${port}/buscar-artigo`);
+//cadastrar 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+function salvarArtigos() {
+    fs.writeFileSync(artigosPath, JSON.stringify(artigos, null, 2));
+}
+
+app.get('/adicionar-artigo', (req, res) => {
+    res.sendFile(path.join(__dirname, 'addartigos.html'));
+});
+
+app.post('/adicionar-artigo', (req, res) => {
+    const NovoArtigo = req.body;
+
+    if (artigos.find(artigos => artigos.nome.toLowerCase() === NovoArtigo.nome.toLowerCase())) {
+        res.send('<h1>Artigo ja existe</h1>');
+        return;
+    }
+
+    artigos.push(NovoArtigo);
+
+    salvarDados();
+    res.send('<h1>artigos add com sucesso</h1>');
+});
+
+
+//home 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.listen(port, () => {
+    console.log(`servidor iniciado em http://localhost:${port}/home`);
 })
---
+++ ac02-JSON/todo.md
@@ -0,0 +1,4 @@
+Um site que oferece informações detalhadas sobre vários
+campos da ciência, como biologia, química, física, etc. Os usuários podem adicionar novos artigos,
+filtrar o conteúdo com base em diferentes nome ou ver todos os artigos cadastrados. 
+[]
